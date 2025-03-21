52a3169238ec16134da6a662b016f679e60e84d1
Clément Voisin <clembs@clembs.com>
2025-03-21 08:39:42 +0100

    refactor(auth): only use cookies for auth instead of php sessions

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -8,27 +8,29 @@ use App\Models\User;
 class Session
 {
   // Durée de vie de la session en secondes (7 jours)
-  private const int SESSION_TEMPS_VIE = 7 * 24 * 60 * 60;
+  private const int SESSION_DURATION = 7 * 24 * 60 * 60;
   // l'identifiant de session et le nom du cookie
-  private const string SESSION_NOM_COOKIE = 'session_token';
+  private const string SESSION_COOKIE_NAME = 'session_token';
 
-  /**
-   * Initialise la session PHP si elle n'est pas déjà démarrée
-   */
-  private static function initSession(): void
+  public static function getSessionToken(): ?string
   {
-    if (session_status() === PHP_SESSION_NONE) {
-      session_start();
+    $sessionToken = $_COOKIE[self::SESSION_COOKIE_NAME];
+
+    if (!isset($sessionToken)) {
+      return null;
     }
+
+    return $sessionToken;
   }
+
 
   /**
    * Crée une nouvelle session pour un utilisateur donné
    */
   public static function create(User $user): void
   {
-    self::initSession();
-    $sessionToken = session_id();
+    // self::initSession();
+    $sessionToken = session_create_id();
 
     $linkpdo = Database::getPDO();
 
@@ -41,7 +43,7 @@ class Session
     $req->execute(['id' => $user->id]);
 
     // On crée une nouvelle session qui expire dans SESSION_TEMPS_VIE
-    $expiresAt = new \DateTime('@' . (time() + self::SESSION_TEMPS_VIE));
+    $expiresAt = new \DateTime('@' . (time() + self::SESSION_DURATION));
     $req = $linkpdo->prepare(
       "INSERT INTO sessions (token, expires_at, user_id) 
                  VALUES (:token, :expires_at, :user_id)"
@@ -53,16 +55,12 @@ class Session
       'user_id' => $user->id,
     ]);
 
-    // On ajoute les informations de session à la session PHP
-    $_SESSION['user_id'] = $user->id;
-    $_SESSION['expires_at'] = $expiresAt->format('Y-m-d H:i:s');
-
     // On crée un cookie de session
     setcookie(
-      self::SESSION_NOM_COOKIE,
+      self::SESSION_COOKIE_NAME,
       $sessionToken,
       [
-        'expires' => time() + self::SESSION_TEMPS_VIE,
+        'expires' => time() + self::SESSION_DURATION,
         'path' => '/',
         'secure' => true,
         'httponly' => true,
@@ -77,22 +75,28 @@ class Session
    */
   public static function getCurrentUser(): ?User
   {
-    self::initSession();
+    $sessionToken = self::getSessionToken();
 
-    if (!isset($_SESSION['user_id']) || !isset($_SESSION['expires_at'])) {
+    if (!$sessionToken) {
       return null;
     }
 
-    $linkpdo = Database::getPDO();
-    $req = $linkpdo->prepare(
-      'SELECT u.* FROM users AS u 
+    $pdo = Database::getPDO();
+    $query = $pdo->prepare(
+      "SELECT 
+      u.id as user_id,
+      u.username as user_username,
+      u.password as user_password,
+      u.created_at as user_created_at,
+      s.expires_at as session_expires_at
+      FROM users AS u 
         JOIN sessions AS s ON u.id = s.user_id 
         WHERE s.token = :token 
-        AND s.expires_at > CURRENT_TIMESTAMP'
+        AND s.expires_at > CURRENT_TIMESTAMP"
     );
 
-    $req->execute(['token' => session_id()]);
-    $res = $req->fetch();
+    $query->execute(['token' => $sessionToken]);
+    $res = $query->fetch();
 
     if (!$res) {
       self::destroy();
@@ -100,15 +104,15 @@ class Session
     }
 
     // si la session expire dans moins d'un jour, on la prolonge
-    if (new \DateTime($res['expires_at']) < new \DateTime('now +1 day')) {
+    if (new \DateTime($res['session_expires_at']) < new \DateTime('now +1 day')) {
       self::extend();
     }
 
     return new User(
-      $res['id'],
-      $res['username'],
-      $res['password'],
-      new \DateTime($res['created_at']),
+      $res['user_id'],
+      $res['user_username'],
+      $res['user_password'],
+      new \DateTime($res['user_created_at']),
     );
   }
 
@@ -121,32 +125,35 @@ class Session
   }
 
   /*
-   * Prolonge la session de SESSION_TEMPS_VIE secondes
+   * Prolonge la session de SESSION_DURATION secondes
    */
   private static function extend(): void
   {
-    $newExpiresAt = new \DateTime('@' . (time() + self::SESSION_TEMPS_VIE));
-    $sessionToken = session_id();
+    $sessionToken = self::getSessionToken();
 
-    $linkpdo = Database::getPDO();
-    $req = $linkpdo->prepare(
+    if (!$sessionToken) {
+      return;
+    }
+
+    $newExpiresAt = new \DateTime('@' . (time() + self::SESSION_DURATION));
+
+    $pdo = Database::getPDO();
+    $query = $pdo->prepare(
       'UPDATE sessions 
-          SET expires_at = :expires_at 
-          WHERE token = :token'
+        SET expires_at = :expires_at 
+        WHERE token = :token'
     );
 
-    $req->execute([
+    $query->execute([
       'expires_at' => $newExpiresAt->format('Y-m-d H:i:s'),
       'token' => $sessionToken
     ]);
 
-    $_SESSION['expires_at'] = $newExpiresAt->format('Y-m-d H:i:s');
-
     setcookie(
-      self::SESSION_NOM_COOKIE,
+      self::SESSION_COOKIE_NAME,
       $sessionToken,
       [
-        'expires' => time() + self::SESSION_TEMPS_VIE,
+        'expires' => time() + self::SESSION_DURATION,
         'path' => '/',
         'secure' => true,
         'httponly' => true,
@@ -160,16 +167,17 @@ class Session
    */
   public static function destroy(): void
   {
-    self::initSession();
+    $sessionToken = self::getSessionToken();
 
-    $linkpdo = Database::getPDO();
-    $req = $linkpdo->prepare('DELETE FROM sessions WHERE token = :token');
-    $req->execute(['token' => session_id()]);
+    if (!$sessionToken) {
+      return;
+    }
 
-    session_unset();
-    session_destroy();
+    $pdo = Database::getPDO();
+    $query = $pdo->prepare('DELETE FROM sessions WHERE token = :token');
+    $query->execute(['token' => $sessionToken]);
 
-    setcookie(self::SESSION_NOM_COOKIE, '', [
+    setcookie(self::SESSION_COOKIE_NAME, '', [
       'expires' => time() - 3600,
       'path' => '/',
       'secure' => true,
