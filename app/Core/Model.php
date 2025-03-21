e230d39283c255c605da74c6d25a5839919fc4e7
Clément Voisin <clembs@clembs.com>
2025-03-21 13:20:39 +0100

    fix(Core): remove all properties for model because theyre not relevant

--- app/Core/Model.php
+++ app/Core/Model.php
@@ -4,14 +4,5 @@ namespace App\Core;
 
 abstract class Model implements \JsonSerializable
 {
-  abstract public static function findById(int $id): ?self;
-
-  /**
-   * @return self[]
-   */
-  abstract public static function findAll(): array;
-
-  abstract public static function delete(int $id): void;
-
   abstract public function jsonSerialize(): array;
 }
